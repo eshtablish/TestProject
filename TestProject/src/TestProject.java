84d98587f999a5e955e77a2d1e7e68071dff583a
de <de@DESKTOP-74RV35E>
2022-06-25 15:34:52 +0530

    First commit

---
+++ TestProject/src/TestProject.java
@@ -0,0 +1,12 @@
+public class TestProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	 
+	        //printing the message
+	        System.out.println("Hello World!");
+	    }
+
+	}
+
+
