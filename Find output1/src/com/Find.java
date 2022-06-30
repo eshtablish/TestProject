c23427e41a57ae68510956a26053873c177967ea
de <de@DESKTOP-74RV35E>
2022-06-30 14:37:27 +0530

    third commit

---
+++ Find output1/src/com/Find.java
@@ -0,0 +1,18 @@
+package com;
+
+public class Find {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=5,b=18;
+
+		if((a=3)==b)
+
+		{ System.out.println(++a);}
+
+		else
+
+		{ System.out.println(++b); }
+	}
+
+}
