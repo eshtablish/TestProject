df9c3dcab21a461319098accd1a9cc90a2017f55
de <de@DESKTOP-74RV35E>
2022-06-30 14:13:58 +0530

    Second commit

---
+++ pre and post increament/src/com/increament.java
@@ -0,0 +1,17 @@
+package com;
+
+public class increament {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 5;
+        int b = 2;
+        int c;
+        int d;
+        c = ++b; 
+        d = a++; 
+        c++; 
+        System.out.println("a = " + a + " b = " + b + " c = " + c + " d = " + d);
+	}
+
+}
