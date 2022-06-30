fcdf314239c17be92df4fe5dcaf710925b912dab
de <de@DESKTOP-74RV35E>
2022-06-30 15:09:56 +0530

    sixth commit

---
+++ Prgm 2/src/com/Demo.java
@@ -0,0 +1,12 @@
+package com;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Y" + "O");
+
+		System.out.print('L' + 'O');
+	}
+
+}
