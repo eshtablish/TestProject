1d5fce13e2d68fc650351b5819b6afd7eee691d4
de <de@DESKTOP-74RV35E>
2022-06-30 14:48:11 +0530

    fourth commit

---
+++ Predict output/src/com/Predict.java
@@ -0,0 +1,26 @@
+package com;
+
+public class Predict {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+			int var1 = 5;
+
+			int var2 = 6;
+
+			if ((var2 = 1) == var1)
+
+			System.out.print(var2);
+
+			else
+
+			System.out.print(++var2);
+
+			}
+
+			
+	}
+
+
