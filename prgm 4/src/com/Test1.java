029c103139942a829ceefd293938adbbecd6b531
de <de@DESKTOP-74RV35E>
2022-06-30 16:53:08 +0530

    seventh commit

---
+++ prgm 4/src/com/Test1.java
@@ -0,0 +1,14 @@
+package com;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float f = (1 / 4) * 10;
+
+		int i =(int) f;
+
+		System.out.println(i);
+	}
+
+}
