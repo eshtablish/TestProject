a8c08ecaed07c5156aa00b01bbf393a14fc6d21b
de <de@DESKTOP-74RV35E>
2022-07-26 11:15:55 +0530

    Abstract Eg 1

---
+++ Abstraction/src/com/Bike.java
@@ -0,0 +1,16 @@
+package com;
+
+abstract class Bike {
+   abstract void run();
+}
+class Honda extends Bike{
+	void run() {
+		System.out.println("running safely");
+	}
+}
+class Demo{
+	public static void main(String args[]) {
+		Honda h=new Honda();
+		h.run();
+	}
+}
